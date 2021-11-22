f026f995e7df4a9535c0f9e36ce846f974936e33
DyingKlokateer <sherwin_navai@protonmail.com>
2021-11-21 23:34:30 -0600

    updated

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-2_c5BWhTxuR', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '367h2qisc08vnbu8km6oeo9env', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'us-east-2' // e.g. us-east-2
+        userPoolId: 'us-west-2_keCSgbtOZ', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: 'sjqofkkmds7adj1louf35doap', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-west-2' // e.g. us-west-2
     },
     api: {
-        invokeUrl: 'https://mfyhaiac02.execute-api.us-east-2.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://qvw88r7jfa.execute-api.us-west-2.amazonaws.com/prod'
     }
 };
